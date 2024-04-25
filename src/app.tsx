8a9590e610ded1f56c7236ea7a3c7dd6e24d7681
Fred Yang <fredyangify@gmail.com>
2024-04-25 17:22:50 -0400

    redid main page

--- src/app.tsx
+++ src/app.tsx
@@ -3,18 +3,15 @@ import {useState} from 'preact/hooks'
 import './app.css'
 
 export function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
             <h1>Fred Yang</h1>
-            <h3>Hello, World!</h3>
-            <div class="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-            </div>
+            <h4>Hey! I'm a creative software engineering student at the University of Waterloo, passionate about exploring math, science, and coding, constantly hungry for new knowledge and challenges.</h4>
 
+            <br/>
+            <br/>
+            <h3>Some fun stuff 👇</h3>
             <a href="fractal-clock/">Fractal Clock</a>
         </>
     )
