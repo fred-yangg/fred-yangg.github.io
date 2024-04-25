d1ccffb57b7b5d105c101c95daa669022ce00fc9
Fred Yang <fredyangify@gmail.com>
2024-04-25 12:21:53 -0400

    changed directory structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+    root: 'src',
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+    },
+    plugins: [preact()],
 })
