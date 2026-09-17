01e130da77e373c9d3e337021714e0d1fc886300
Fred Yang <fredyang@noreply.com>
2026-09-17 00:23:45 -0700

    weight intro spawn time by ease(1 - level/levels)

--- src/fractal-clock/intro.ts
+++ src/fractal-clock/intro.ts
@@ -31,7 +31,7 @@ function easeInCubic(t: number) {
 }
 
 function spawnWeight(depth: number, levels: number) {
-    return 1 - easeInCubic(depth / levels)
+    return easeInCubic(1 - depth / levels)
 }
 
 function spawnAt(elapsed: number, total: number, levels: number) {
